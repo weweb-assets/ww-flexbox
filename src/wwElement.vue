ab64c01c0beeef7b87d1306764729d972ccab30c
Aurélie VIOLETTE <orwel.violette@gmail.com>
2021-03-12 18:13:11 +0100

    First commit

---
+++ src/wwElement.vue
@@ -0,0 +1,132 @@
+<template>
+    <div class="ww-flexbox">
+        <Paginator v-if="content.pagination === 'top'" class="paginator"></Paginator>
+        <wwLayout
+            class="ww-flexbox__dropzone"
+            path="children"
+            :direction="content.direction"
+            :style="style"
+            :start="start"
+            :pagination="!!content.pagination"
+            :max="content.maxItem"
+            @update:total="total = $event"
+            ww-responsive="wwLayout"
+        ></wwLayout>
+        <Paginator v-if="content.pagination === 'bottom'" class="paginator"></Paginator>
+        <!-- wwEditor:start -->
+        <div class="ww-flexbox__menu">
+            <wwEditorIcon small name="config"></wwEditorIcon>
+        </div>
+        <!-- wwEditor:end -->
+    </div>
+</template>
+
+<script>
+import Paginator from './Paginator.vue';
+import { getConfiguration } from './config.js';
+
+export default {
+    name: '__COMPONENT_NAME__',
+    components: { Paginator },
+    wwDefaultContent: {
+        children: [],
+        reverse: wwLib.responsive(false),
+        direction: wwLib.responsive('row'),
+        justifyContent: wwLib.responsive('center'),
+        alignItems: wwLib.responsive('start'),
+        maxItem: wwLib.responsive(50),
+        pagination: wwLib.responsive(null),
+    },
+    wwEditorConfiguration({ content }) {
+        return getConfiguration(content);
+    },
+    props: {
+        content: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: Object,
+        /* wwEditor:end */
+    },
+    data() {
+        return {
+            start: 0,
+            total: 0,
+        };
+    },
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            return false;
+        },
+        style() {
+            return {
+                flexDirection: this.content.direction,
+                justifyContent: this.content.justifyContent,
+                alignItems: this.content.direction === 'row' ? this.content.alignItems : 'stretch',
+            };
+        },
+    },
+    watch: {
+        'content.pagination'(isPaginated, wasPaginated) {
+            if (this.wwEditorState.isACopy) {
+                return;
+            }
+            if (isPaginated && !wasPaginated && !this.content.maxItem) {
+                this.$emit('update-effect', { maxItem: 20 });
+            }
+        },
+        'content.maxItem'(newVal, oldVal) {
+            if (this.wwEditorState.isACopy) {
+                return;
+            }
+            if (!newVal && oldVal && this.content.pagination) {
+                this.$emit('update-effect', { pagination: null });
+            }
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.ww-flexbox {
+    position: relative;
+    box-sizing: border-box;
+    &__dropzone {
+        display: flex;
+        flex-wrap: wrap;
+        height: 100%;
+        width: 100%;
+    }
+    .ww-flexbox__menu {
+        pointer-events: auto;
+        opacity: 0;
+        pointer-events: none;
+        display: flex;
+        position: absolute;
+        border-radius: 100%;
+        padding: var(--ww-spacing-01);
+        transition: opacity 0.2s ease;
+        z-index: 11;
+        cursor: pointer;
+        background-color: var(--ww-color-blue-500);
+        color: white;
+        justify-content: center;
+        align-items: center;
+        left: 0;
+        top: 0;
+        transform: translate(-50%, -50%);
+        transition: transform 0.3s ease;
+        &:hover {
+            transform: translate(-50%, -50%) scale(1.3);
+            opacity: 1;
+        }
+    }
+    &:hover {
+        > .ww-flexbox__menu {
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+}
+</style>
